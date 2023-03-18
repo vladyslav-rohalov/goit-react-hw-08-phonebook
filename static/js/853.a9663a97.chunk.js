e50e0758423bd53d597615ed7c1f41b97d8d6a95
vladyslav-rohalov <113693621+vladyslav-rohalov@users.noreply.github.com>
2023-03-18 10:20:06 +0000

    Deploying to gh-pages from @ vladyslav-rohalov/goit-react-hw-08-phonebook@613eda4e6b5a964f52cf939c97b0bad74f3780da 🚀

---
+++ static/js/853.a9663a97.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_redux=self.webpackChunkreact_homework_redux||[]).push([[853],{3853:function(t,r,e){e.d(r,{Imn:function(){return o}});var n=e(9983);function o(t){return(0,n.w_)({tag:"svg",attr:{viewBox:"0 0 24 24",fill:"none",stroke:"currentColor",strokeWidth:"2",strokeLinecap:"round",strokeLinejoin:"round"},child:[{tag:"path",attr:{d:"M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2z"}},{tag:"polyline",attr:{points:"22,6 12,13 2,6"}}]})(t)}}}]);
+//# sourceMappingURL=853.a9663a97.chunk.js.map
